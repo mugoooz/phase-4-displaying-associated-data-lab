fb7911930d67ea6e547267ff4297ded028763372
Eric Mugo <mugoericpg20@gmail.com>
2023-06-28 13:03:48 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        items = user.items
+        render json: { user: user, items: items }
+
+    end
+
 end
